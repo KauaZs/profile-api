d135502b39890100c9454f22b1f13acc21dd2d33
KauaZs <zeratop89@gmail.com>
2025-01-07 12:46:49 -0300

    fix code

--- src/routes/oauth2/user.ts
+++ src/routes/oauth2/user.ts
@@ -23,7 +23,7 @@ export default async function getUserRoute(req: FastifyRequest, res: FastifyRepl
         
         return res.status(200).send(response)
     } catch(e: any) {
-        return res.status(403).send({
+        return res.status(401).send({
             "error": "not authorized",
             "status": 401
         })
