4f50c40767624635ca77e6254d6edb3de7adf04b
KauaZs <zeratop89@gmail.com>
2025-01-07 09:59:53 -0300

    implement connect mongo

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,18 @@ import fastifyCookie from '@fastify/cookie';
 import Fastify from 'fastify';
 import Router from './routes/RouterManager';
 import fastifyCors from '@fastify/cors';
+import connectMongo from './utils/connectDatabase';
+import dotenv from 'dotenv';
 
 const fastify = Fastify()
+dotenv.config()
 
 fastify.register(fastifyCookie)
 fastify.register(fastifyCors)
 
 fastify.listen({ port: 3001 }, () => {
-    console.log('[union-api] [stats] api online')
+    console.log('[profile-api] [stats] api online')
+    connectMongo(process.env.MONGOSRV as string)
 })
 
 Router(fastify);
