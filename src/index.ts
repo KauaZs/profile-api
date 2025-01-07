94b2bab05db09fdb5661843a57a4da4f750db105
KauaZs <zeratop89@gmail.com>
2025-01-07 11:13:14 -0300

    add package zod

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import fastifyCookie from '@fastify/cookie';
-import Fastify from 'fastify';
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify';
 import Router from './routes/RouterManager';
 import fastifyCors from '@fastify/cors';
 import connectMongo from './utils/connectDatabase';
@@ -15,5 +15,6 @@ fastify.listen({ port: 3001 }, () => {
     console.log('[profile-api] [stats] api online')
     connectMongo(process.env.MONGOSRV as string)
 })
+
 
 Router(fastify);
