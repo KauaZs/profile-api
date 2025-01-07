657f9133bee219e19f5e5a44664eb261c123da36
KauaZs <zeratop89@gmail.com>
2025-01-07 11:12:37 -0300

    feat: add change profile route

--- src/routes/user/changeProfile.ts
+++ src/routes/user/changeProfile.ts
@@ -1,5 +1,72 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import dotenv from 'dotenv';
+import User from "../../models/User";
+import { z } from "zod";
 
-export default async function changeProfile(req: FastifyRequest, res: FastifyReply) {
+interface query {
+    userId: string
+}
+
+dotenv.config()
+export default async function changeProfile(req: FastifyRequest<{Querystring: query}>, res: FastifyReply) {
+    const data = req.body;
+    const userId = req.query.userId;
+
+    const authorization = req.headers.authorization;
+    if (!authorization) return res.status(401).send({ "error": "request not authorized", "status": 401})
+    if (authorization !== process.env.API_KEY) return res.status(401).send({ "error": "request not authorized", "status": 401})
+
+    try {
+        if (!userId) return res.status(401).send({
+            "error": "userid invalid",
+            "status": 400
+        });
+
+        if (!data) return res.status(401).send({
+            "error": "body invalid",
+            "status": 400
+        })
+
+        const profileOptionsSchema = z.object({
+            displayName: z.string().max(50).optional(),
+            avatar: z.string().url().optional(),
+            banner: z.string().url().optional(),
+            aboutme: z.string().max(200).optional(),
+            colorBakcground: z.string().regex(/^#[0-9A-Fa-f]{6}$/).optional(),
+            colorCard: z.string().regex(/^#[0-9A-Fa-f]{6}$/).optional(),
+            effectSpace: z.boolean().optional(),
+        });
+
+        const updateUserSchema = z.object({
+            profileOptions: profileOptionsSchema.partial().optional(),
+        });
+
+        const validatedData = updateUserSchema.parse(req.body) as any;
+        const updateFields: {[key: string]: any} = {};
+
+        if (validatedData?.profileOptions) {
+            for (const key in validatedData?.profileOptions) {
+                updateFields[`profileOptions.${key}`] = validatedData.profileOptions[key];
+            }
+        }
+        const updatedUser = await User.findByIdAndUpdate(
+            userId, 
+            { $set: updateFields }, 
+            { runValidators: true }
+        );
+    
+        res.status(200)
+            .send({
+                "message": "update",
+                "status": 200
+            })
+    } catch(e: any) {
+        if (e instanceof z.ZodError) {
+            return res.status(400).send({
+                error: 'invalid info',
+                details: e.errors.map((e) => e.message),
+            });
+        }
+    }
     
 }
