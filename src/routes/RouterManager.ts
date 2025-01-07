b75f038c2c2edcfe8fecbb76e657f191da5599c3
KauaZs <zeratop89@gmail.com>
2025-01-07 11:13:01 -0300

    feat: add route

--- src/routes/RouterManager.ts
+++ src/routes/RouterManager.ts
@@ -6,9 +6,10 @@ import * as botController from '../controllers/profileControler'
 
 */
 export default async function Router(fastify: FastifyInstance) {
-    fastify.get('/api/profile', botController.changeProfile)
     fastify.get('/api/auth/callback', botController.callbackRoute)
     fastify.get('/api/auth', botController.authRoute)
     fastify.get('/api/auth/user', botController.getUserRoute)
     fastify.get('/api/auth/logout', botController.logoutRoute)
+
+    fastify.post('/api/profile', botController.changeProfile)
 }
