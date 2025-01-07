87d0dde7a72f679cd612087df33db5dc378f1464
KauaZs <zeratop89@gmail.com>
2025-01-07 09:58:51 -0300

    feat: add logout route

---
+++ src/routes/oauth2/logout.ts
@@ -0,0 +1,20 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export default async function logoutRoute(req: FastifyRequest, res: FastifyReply) {
+    const user = req.cookies.user_discord;
+
+    if (!user) return res.status(401)
+        .send({
+            "error": "you are not logged in",
+            "status": 401
+        })
+
+    res.clearCookie('user_discord', {
+        path: '/'
+    })
+    .status(200)
+    .send({
+        "message": "account logged out",
+        "status": 200
+    })
+}
