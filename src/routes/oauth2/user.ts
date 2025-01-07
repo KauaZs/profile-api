b813823fed0d76422ecab9ef639c05704e021487
KauaZs <zeratop89@gmail.com>
2025-01-07 09:58:45 -0300

    chane: changed response add database

--- src/routes/oauth2/user.ts
+++ src/routes/oauth2/user.ts
@@ -1,6 +1,8 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import getUserData from "../../utils/database/getUserData";
+import OAuth from "discord-oauth2";
 
 dotenv.config()
 export default async function getUserRoute(req: FastifyRequest, res: FastifyReply) {
@@ -11,8 +13,15 @@ export default async function getUserRoute(req: FastifyRequest, res: FastifyRepl
     });
     
     try {
-        const user = jwt.verify(user_access, process.env.SECRET_KEY as string)
-        return res.status(200).send(user)
+        const user = jwt.verify(user_access, process.env.SECRET_KEY as string) as OAuth.User
+        const userData = await getUserData(user?.id as string, user)
+
+        const response = {
+            discord: user,
+            database: userData
+        }
+        
+        return res.status(200).send(response)
     } catch(e: any) {
         return res.status(403).send({
             "error": "not authorized",
