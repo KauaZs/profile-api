4639413b32ba171a1074f9449932dcbccdacb837
KauaZs <zeratop89@gmail.com>
2025-01-08 15:10:56 -0300

    feat: add cooldowns schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -16,28 +16,36 @@ interface profilePreferences {
     socials: ISocials
 }
 
+interface Cooldowns {
+    updateUsername: number
+}
+
 interface userProps {
     _id: string,
     accountCreatedAt: Date,
-    profileOptions: profilePreferences
+    profileOptions: profilePreferences,
+    cooldowns: Cooldowns
 }
 
 const userModel = new Schema<userProps>({
     _id: { type: String, required: true, unique: true },
     accountCreatedAt: { type: Date, default: new Date() },
+    cooldowns: {
+        updateUsername: { type: Number, default: 0 }
+    },
     profileOptions: {
         displayName: { type: String },
         avatar: { type: String },
         banner: { type: String },
         aboutme: { type: String, default: 'Hello, welcome to my profile' },
-        colorBakckground: { type: String, default: '#111827' },
+        colorBackground: { type: String, default: '#111827' },
         colorCard: { type: String, default: '#1f2937' },
-        effectSpace: { type: Boolean, default: true },
+        effectSpace: { type: Boolean, default: false },
         socials: {
             github: { type: String, default: '' },
             discord: { type: String, default: '' }
         }
-    }
+    },
 }, {
     versionKey: false
 })
