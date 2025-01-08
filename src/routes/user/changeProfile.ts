b3d83fff44e02a19e8fb736cf47a2517ca62e39a
KauaZs <zeratop89@gmail.com>
2025-01-08 15:10:15 -0300

    remove validator

--- src/routes/user/changeProfile.ts
+++ src/routes/user/changeProfile.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import User from "../../models/User";
 import { z } from "zod";
 
+
 interface query {
     userId: string
 }
@@ -13,9 +14,8 @@ export default async function changeProfile(req: FastifyRequest<{Querystring: qu
     const userId = req.query.userId;
 
     const authorization = req.headers.authorization;
-    if (!authorization) return res.status(401).send({ "error": "request not authorized", "status": 401})
-    if (authorization !== process.env.API_KEY) return res.status(401).send({ "error": "request not authorized", "status": 401})
-
+    if (!authorization) return res.status(401).send({ "error": "request notw authorized", "status": 401})
+    if (authorization !== process.env.API_KEY) return res.status(401).send({ "error": "request notz authorized", "status": 401})
     try {
         if (!userId) return res.status(401).send({
             "error": "userid invalid",
@@ -26,38 +26,29 @@ export default async function changeProfile(req: FastifyRequest<{Querystring: qu
             "error": "body invalid",
             "status": 400
         })
-
-        const profileOptionsSchema = z.object({
-            displayName: z.string().max(50).optional(),
-            avatar: z.string().url().optional(),
-            banner: z.string().url().optional(),
-            aboutme: z.string().max(200).optional(),
-            colorBackground: z.string().regex(/^#[0-9A-Fa-f]{6}$/).optional(),  
-            colorCard: z.string().regex(/^#[0-9A-Fa-f]{6}$/).optional(),
-            effectSpace: z.boolean().optional(),
-            socials: z.object({
-                github: z.string().url().optional(),
-                discord: z.string().url().optional(),
-            })
-         });
         
-        const updateUserSchema = z.object({
-            profileOptions: profileOptionsSchema.partial().optional(),
-        });
-        
-    
-        const validatedData = updateUserSchema.parse(req.body) as any;
+        const validatedData = req.body as any
+        const userData = await User.findById(userId)
+        const updateFields: {[key: string]: any} = {};
 
-        if (validatedData?.profileOptions?.displayName) {
+        if (userData?.profileOptions.displayName !== validatedData?.profileOptions?.displayName) {
+            const cooldown = userData?.cooldowns?.updateUsername as number;
+           
+            if (cooldown > Date.now()) return res.status(400)
+                .send({
+                    "error": `you changed your nickname recently, please wait ${Math.floor((userData?.cooldowns?.updateUsername! - Date.now()) / 60000)} minutes`,
+                    "status": 400
+            });
+            
             const findName = await User.findOne({ 'profileOptions.displayName': validatedData?.profileOptions?.displayName });
             if (findName) return res.status(400)
                 .send({
                     "error": "This name is already being used",
                     "status": 400
                 })
-        }
 
-        const updateFields: {[key: string]: any} = {};
+            updateFields['cooldowns.updateUsername'] = Date.now() + (60000 * 30);
+        }
 
         if (validatedData?.profileOptions) {
             for (const key in validatedData?.profileOptions) {
@@ -69,7 +60,7 @@ export default async function changeProfile(req: FastifyRequest<{Querystring: qu
             { $set: updateFields }, 
             { runValidators: true }
         );
-    
+        
         res.status(200)
             .send({
                 "message": "update",
