881dfd295b49b2eb5b4a1dee64617e4b24fbd7bd
KauaZs <zeratop89@gmail.com>
2025-01-09 07:46:55 -0300

    fix url

--- src/utils/auth2Client.ts
+++ src/utils/auth2Client.ts
@@ -5,5 +5,5 @@ dotenv.config()
 export const DiscordAuth = new DiscordOauth2({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: "http://localhost:3001/api/auth/callback",
+    redirectUri: `${process.env.API_URL}/api/auth/callback`,
 })
