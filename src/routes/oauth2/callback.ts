cb3ba89ca951fbf12092d181f50db7ac8f3c83f6
KauaZs <zeratop89@gmail.com>
2025-01-09 10:56:17 -0300

    aas

--- src/routes/oauth2/callback.ts
+++ src/routes/oauth2/callback.ts
@@ -31,7 +31,8 @@ export default async function callbackRoute(req: FastifyRequest<{Querystring: qu
     res
         .setCookie('user_discord', user_token, {
             path: '/',
-            maxAge: 7 * 24 * 60 * 60 * 1000
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            domain: 'kauazs.tech'
         })
        // .redirect('https://kaurds.kauazs.tech/u/' + userData.profileOptions.displayName)
         
