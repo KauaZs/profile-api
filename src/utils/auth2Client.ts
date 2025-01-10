715e88e3debed07721d06ad0fb13e5e51fb7ef2c
KauaZs <zeratop89@gmail.com>
2025-01-10 10:08:30 -0300

    fix link using env

--- src/utils/auth2Client.ts
+++ src/utils/auth2Client.ts
@@ -5,5 +5,5 @@ dotenv.config()
 export const DiscordAuth = new DiscordOauth2({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: `https://kaurds.kauazs.tech/api/auth/callback`,
+    redirectUri: `https://${process.env.WEBSITE_URL}/api/auth/callback`,
 })
