90caf3bbf963ff7b32b34a30ac1ce051f6bc5c94
KauaZs <zeratop89@gmail.com>
2025-01-10 10:09:42 -0300

    update routes

--- src/controllers/profileControler.ts
+++ src/controllers/profileControler.ts
@@ -2,8 +2,9 @@ import authRoute from "../routes/oauth2/auth";
 import callbackRoute from "../routes/oauth2/callback";
 import logoutRoute from "../routes/oauth2/logout";
 import getUserRoute from "../routes/oauth2/user";
-import changeProfile from "../routes/user/changeProfile";
-import findUserRoute from "../routes/user/findUser";
+import changeProfile from "../routes/users/changeProfile";
+import findUserRoute from "../routes/users/findUser";
+import listHallFame from "../routes/users/listHallFame";
 
 export {
     changeProfile,
@@ -11,5 +12,6 @@ export {
     callbackRoute,
     getUserRoute,
     logoutRoute,
-    findUserRoute
+    findUserRoute,
+    listHallFame
 }
