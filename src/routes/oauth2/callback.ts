38d89f4fda72b883a1c082372dad8260e95a12ca
KauaZs <zeratop89@gmail.com>
2025-01-11 07:00:00 -0300

    feat add maxage cookie

--- src/routes/oauth2/callback.ts
+++ src/routes/oauth2/callback.ts
@@ -31,6 +31,7 @@ export default async function callbackRoute(req: FastifyRequest<{Querystring: qu
     res
         .setCookie('user_discord', user_token, {
             path: '/',
+            maxAge: 604800
         })
     
         .redirect('https://kaurds.kauazs.tech/u/' + userData.profileOptions.displayName)
